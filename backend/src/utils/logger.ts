437d5abd83f24ac314efe336847897962b2b83bc
desarrolladorcsjt <desarrollador.csjt@gmail.com>
2025-07-11 13:47:43 -0500

    cambio

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -2,7 +2,9 @@ import pino from "pino";
 
 const logger = pino({
   prettyPrint: {
-    ignore: "pid,hostname"
+    colorize: true,
+    translateTime: 'dd-mm-yyyy HH:MM:ss',
+    ignore: 'pid,hostname'
   }
 });
 
